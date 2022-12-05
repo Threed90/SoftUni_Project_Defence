026220fbb97b05d0ed2f63774375eff8046d3a40
Threed90 <threed@abv.bg>
2022-12-05 22:38:48 +0200

    Add "to do" description in index controller.

--- Tellers/Tellers.App/Controllers/HomeController.cs
+++ Tellers/Tellers.App/Controllers/HomeController.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
-namespace Tellers.App.Controllers
+﻿namespace Tellers.App.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         public IActionResult Index()
         {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                // do redirect logic here
+
+                // maybe add logger info
+            }
+
             return View();
         }
     }
