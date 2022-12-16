6dbf1461b96bf97dc7d83a94cc59737fc0f1f299
Threed90 <threed@abv.bg>
2022-12-16 02:59:47 +0200

    Creating profile view template, no adding logic inside.

---
+++ Tellers/Tellers.App/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tellers.ViewModels.Profiles;
+
+namespace Tellers.App.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Authorize]
+        public IActionResult Info(string profileId)
+        {
+            return View(new ProfileInfoViewModel()
+            {
+                UserId= profileId, //has to be userId not profileId
+            });
+        }
+    }
+}
---
+++ Tellers/Tellers.ViewModels/Profiles/ProfileInfoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tellers.ViewModels.Profiles
+{
+    public class ProfileInfoViewModel : ProfileViewModel
+    {
+    }
+}
